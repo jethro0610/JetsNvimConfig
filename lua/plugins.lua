05300cc0858e715cb1f6e66fdf66360e168e5d04
jethro0610 <jethro0610@outlook.com>
2023-04-07 13:45:16 -0700

    Add LSP with clangd

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,7 @@
 return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
     use 'sainnhe/everforest'
+    use 'neovim/nvim-lspconfig'
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
