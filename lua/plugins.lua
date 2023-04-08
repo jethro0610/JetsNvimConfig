c61a6e928ea45bfd3e5937b05cfce2c19fcfc3f0
jethro0610 <jethro0610@outlook.com>
2023-04-07 19:25:45 -0700

    Add nvim-vmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,12 @@ return require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
     use 'sainnhe/everforest'
     use 'neovim/nvim-lspconfig'
+
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/vim-vsnip'
+
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,1 +1,28 @@
-require'lspconfig'.clangd.setup{}
+local vim = vim
+local lsp = require 'lspconfig'
+local cmp = require 'cmp'
+
+vim.opt.signcolumn = 'yes'
+lsp.clangd.setup{}
+
+cmp.setup({
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' }
+    }),
+    mapping = {
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end
+    }
+})
